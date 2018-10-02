d012961ab01b2be6a162eec9476eeaff23104946
Haruhiko Imatake <imatake_haruhiko@gs2.io>
2018-10-02 16:43:06 +0900

    List<StringHolder> に char 配列を渡して要素追加できるように

--- src/gs2/core/util/List.hpp
+++ src/gs2/core/util/List.hpp
@@ -19,6 +19,7 @@
 
 #include "../Gs2Object.hpp"
 #include "StandardAllocator.hpp"
+#include "StringHolder.hpp"
 #include <vector>
 
 GS2_START_OF_NAMESPACE
@@ -113,6 +114,106 @@ public:
         m_List.emplace_back(std::move(element));
         return *this;
     }
+};
+
+template<> class List<StringHolder> : public Gs2Object {
+
+private:
+
+    std::vector<StringHolder, detail::StandardAllocator<StringHolder>>m_List;
+
+public:
+
+    List()
+    {}
+
+    List(const List& obj) :
+        Gs2Object(obj),
+        m_List(obj.m_List)
+    {
+    }
+
+    List(List&& obj) :
+        Gs2Object(std::move(obj)),
+        m_List(std::move(obj.m_List))
+    {
+    }
+
+    ~List() {
+    }
+
+    List& operator=(const List& list)
+    {
+        Gs2Object::operator=(list);
+
+        if (&list != this)
+        {
+            m_List.clear();
+            for (const auto& element : list.m_List)
+            {
+                m_List.emplace_back(element);
+            }
+        }
+
+        return *this;
+    }
+
+    List& operator=(List&& list)
+    {
+        Gs2Object::operator=(std::move(list));
+
+        if (&list != this)
+        {
+            m_List = std::move(list.m_List);
+        }
+
+        return *this;
+    }
+
+    const List* operator->() const
+    {
+        return this;
+    }
+
+    List* operator->()
+    {
+        return this;
+    }
+
+    const Int32 getCount() const
+    {
+        return m_List.size();
+    }
+
+    const StringHolder& operator[](Int32 i) const
+    {
+        return m_List[i];
+    }
+
+    void clear()
+    {
+        m_List.clear();
+    }
+
+    List& operator+=(const StringHolder& element)
+    {
+        m_List.emplace_back(element);
+        return *this;
+    }
+
+    List& operator+=(StringHolder&& element)
+    {
+        m_List.emplace_back(std::move(element));
+        return *this;
+    }
+
+    List& operator+=(const Char element[])
+    {
+        m_List.emplace_back(element);
+        return *this;
+    }
+
+    // TODO: move
 };
 
 GS2_END_OF_NAMESPACE
