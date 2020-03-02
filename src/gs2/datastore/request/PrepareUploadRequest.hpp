58dc1c228b0be11865eaff8ee8493a13196e6f75
gs2-ci <ci@gs2.io>
2020-03-02 09:44:17 +0000

    [gs2-datastore] Service/test seeds modified
    gs2-datastore: 937fde08252c365c71ca8cff424bb69e17525be0
    gs2-sdk-gen: acd0876006932e81c812917c25994f12306ca040
    gs2-test-gen: 8b0576abbc396fa9e58ede6163da50046105b6c4

--- src/gs2/datastore/request/PrepareUploadRequest.hpp
+++ src/gs2/datastore/request/PrepareUploadRequest.hpp
@@ -47,6 +47,8 @@ private:
         optional<StringHolder> accessToken;
         /** ネームスペース名 */
         optional<StringHolder> namespaceName;
+        /** データの名前 */
+        optional<StringHolder> name;
         /** アップロードするデータの MIME-Type */
         optional<StringHolder> contentType;
         /** ファイルのアクセス権 */
@@ -62,6 +64,7 @@ private:
             Gs2BasicRequest::Data(data),
             accessToken(data.accessToken),
             namespaceName(data.namespaceName),
+            name(data.name),
             contentType(data.contentType),
             scope(data.scope),
             duplicationAvoider(data.duplicationAvoider)
@@ -176,6 +179,37 @@ public:
     PrepareUploadRequest& withNamespaceName(StringHolder namespaceName)
     {
         ensureData().namespaceName.emplace(std::move(namespaceName));
+        return *this;
+    }
+
+    /**
+     * データの名前を取得
+     *
+     * @return データの名前
+     */
+    const optional<StringHolder>& getName() const
+    {
+        return ensureData().name;
+    }
+
+    /**
+     * データの名前を設定
+     *
+     * @param name データの名前
+     */
+    void setName(StringHolder name)
+    {
+        ensureData().name.emplace(std::move(name));
+    }
+
+    /**
+     * データの名前を設定
+     *
+     * @param name データの名前
+     */
+    PrepareUploadRequest& withName(StringHolder name)
+    {
+        ensureData().name.emplace(std::move(name));
         return *this;
     }
 
