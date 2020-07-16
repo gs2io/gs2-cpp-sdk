86d8d0a53460a726fb64b22cd9448dcfa9cf559b
gs2-ci <ci@gs2.io>
2020-07-16 23:38:44 +0000

    [gs2-job-queue] Service/test seeds modified
    gs2-job-queue: 0d34bf0290dcbd6663f081d11b8a11d6eaf08370
    gs2-sdk-gen: 744610074a159c88dfc47eb2c51185109e8e0f07
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/jobQueue/Gs2JobQueueWebSocketClient.hpp
+++ src/gs2/jobQueue/Gs2JobQueueWebSocketClient.hpp
@@ -1105,6 +1105,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
