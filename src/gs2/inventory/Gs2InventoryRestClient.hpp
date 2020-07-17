7786ce2b6806dbd4337d088f4b2c83704dbbd975
gs2-ci <ci@gs2.io>
2020-07-17 02:58:59 +0000

    [gs2-inventory] Service/test seeds modified
    gs2-inventory: 50e77ac78103fbb24a40b0d8c79f6e3e7c088190
    gs2-sdk-gen: 744610074a159c88dfc47eb2c51185109e8e0f07
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/inventory/Gs2InventoryRestClient.hpp
+++ src/gs2/inventory/Gs2InventoryRestClient.hpp
@@ -4003,6 +4003,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
