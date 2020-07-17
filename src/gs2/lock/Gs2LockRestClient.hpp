d02875271a44c15bf876cb13dd7cf6651d6135f2
gs2-ci <ci@gs2.io>
2020-07-17 09:58:40 +0000

    [gs2-lock] Service/test seeds modified
    gs2-lock: da3717f6e209f92dc328031caf30f33ab0acfaf4
    gs2-sdk-gen: f044dbf11c44de7a9064d61e0ae357c82ce69808
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/lock/Gs2LockRestClient.hpp
+++ src/gs2/lock/Gs2LockRestClient.hpp
@@ -1022,6 +1022,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
