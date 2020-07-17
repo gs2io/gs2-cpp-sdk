24700b0eb2c1dd0bf9c1a93122d0fe1f5d0f586e
gs2-ci <ci@gs2.io>
2020-07-17 14:53:16 +0000

    [gs2-formation2] Service/test seeds modified
    gs2-formation2: 803d1fb9c9b538c6c76b712883b4631a0df4295a
    gs2-sdk-gen: 542771d52a65f9300868299ee7a466dd41f66805
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/formation/Gs2FormationWebSocketClient.hpp
+++ src/gs2/formation/Gs2FormationWebSocketClient.hpp
@@ -3083,6 +3083,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
