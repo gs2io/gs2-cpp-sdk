abe5ac0c35cfbdd63f300f9017384d785a1c4fff
gs2-ci <ci@gs2.io>
2020-07-17 12:43:08 +0000

    [gs2-realtime] Service/test seeds modified
    gs2-realtime: 2d4394f17b4548c5642e29c3728ef7d341c4cca5
    gs2-sdk-gen: 542771d52a65f9300868299ee7a466dd41f66805
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/realtime/model/Namespace.hpp
+++ src/gs2/realtime/model/Namespace.hpp
@@ -61,6 +61,8 @@ private:
         optional<NotificationSetting> createNotification;
         /** ログの出力設定 */
         optional<LogSetting> logSetting;
+        /** None */
+        optional<StringHolder> status;
         /** 作成日時 */
         optional<Int64> createdAt;
         /** 最終更新日時 */
@@ -76,6 +78,7 @@ private:
             description(data.description),
             serverType(data.serverType),
             serverSpec(data.serverSpec),
+            status(data.status),
             createdAt(data.createdAt),
             updatedAt(data.updatedAt)
         {
@@ -156,6 +159,13 @@ private:
                     const auto& jsonObject = detail::json::getObject(jsonValue);
                     this->logSetting.emplace();
                     detail::json::JsonParser::parse(&this->logSetting->getModel(), jsonObject);
+                }
+            }
+            else if (std::strcmp(name_, "status") == 0)
+            {
+                if (jsonValue.IsString())
+                {
+                    this->status.emplace(jsonValue.GetString());
                 }
             }
             else if (std::strcmp(name_, "createdAt") == 0)
@@ -449,6 +459,37 @@ public:
     }
 
     /**
+     * Noneを取得
+     *
+     * @return None
+     */
+    const optional<StringHolder>& getStatus() const
+    {
+        return ensureData().status;
+    }
+
+    /**
+     * Noneを設定
+     *
+     * @param status None
+     */
+    void setStatus(StringHolder status)
+    {
+        ensureData().status.emplace(std::move(status));
+    }
+
+    /**
+     * Noneを設定
+     *
+     * @param status None
+     */
+    Namespace& withStatus(StringHolder status)
+    {
+        setStatus(std::move(status));
+        return *this;
+    }
+
+    /**
      * 作成日時を取得
      *
      * @return 作成日時
@@ -554,6 +595,10 @@ inline bool operator!=(const Namespace& lhs, const Namespace& lhr)
             return true;
         }
         if (lhs.m_pData->logSetting != lhr.m_pData->logSetting)
+        {
+            return true;
+        }
+        if (lhs.m_pData->status != lhr.m_pData->status)
         {
             return true;
         }
