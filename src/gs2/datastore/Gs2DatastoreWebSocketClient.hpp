2771e5e5c7a36b065d6aaa208481d821c4f3d676
gs2-ci <ci@gs2.io>
2020-07-17 13:05:49 +0000

    [gs2-datastore] Service/test seeds modified
    gs2-datastore: 9b73c25bb3f3ad9ef13aa0ea8dd8c49435135961
    gs2-sdk-gen: 542771d52a65f9300868299ee7a466dd41f66805
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/datastore/Gs2DatastoreWebSocketClient.hpp
+++ src/gs2/datastore/Gs2DatastoreWebSocketClient.hpp
@@ -2205,6 +2205,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
