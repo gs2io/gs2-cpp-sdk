94953b0a78a1cc552398ad7f1f30e16a93ee2758
gs2-ci <ci@gs2.io>
2020-07-17 14:29:44 +0000

    [gs2-lottery] Service/test seeds modified
    gs2-lottery: bf9f4939845817661289c4c9736c8f9caf472e15
    gs2-sdk-gen: 542771d52a65f9300868299ee7a466dd41f66805
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/lottery/Gs2LotteryWebSocketClient.hpp
+++ src/gs2/lottery/Gs2LotteryWebSocketClient.hpp
@@ -2374,6 +2374,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
