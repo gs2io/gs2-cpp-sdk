f23e4a74844d34159bfb0887a7737c58fe414b3e
gs2-ci <ci@gs2.io>
2020-07-17 13:00:23 +0000

    [gs2-version] Service/test seeds modified
    gs2-version: 86a42da5cf82f04ed2c3482e68072f7031b719b4
    gs2-sdk-gen: 542771d52a65f9300868299ee7a466dd41f66805
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/version/Gs2VersionRestClient.hpp
+++ src/gs2/version/Gs2VersionRestClient.hpp
@@ -1848,6 +1848,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
