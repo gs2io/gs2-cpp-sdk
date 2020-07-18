eac6241bbb4d69f20b8494270faed1eeb88eade3
gs2-ci <ci@gs2.io>
2020-07-18 08:04:15 +0000

    [gs2-matchmaking] Service/test seeds modified
    gs2-matchmaking: 910f8d1b0445b97c03d7224dd6a382af88a19b20
    gs2-sdk-gen: 542771d52a65f9300868299ee7a466dd41f66805
    gs2-test-gen: be182f3c6bfaef46610c076807755b890ba29e63

--- src/gs2/matchmaking/Gs2MatchmakingRestClient.hpp
+++ src/gs2/matchmaking/Gs2MatchmakingRestClient.hpp
@@ -2761,6 +2761,11 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
+        if (obj.getStatus())
+        {
+            jsonWriter.writePropertyName("status");
+            jsonWriter.writeCharArray(*obj.getStatus());
+        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
--- src/gs2/matchmaking/model/Namespace.hpp
+++ src/gs2/matchmaking/model/Namespace.hpp
@@ -77,6 +77,8 @@ private:
         optional<NotificationSetting> completeNotification;
         /** ログの出力設定 */
         optional<LogSetting> logSetting;
+        /** None */
+        optional<StringHolder> status;
         /** 作成日時 */
         optional<Int64> createdAt;
         /** 最終更新日時 */
@@ -97,6 +99,7 @@ private:
             completeMatchmakingTriggerType(data.completeMatchmakingTriggerType),
             completeMatchmakingTriggerRealtimeNamespaceId(data.completeMatchmakingTriggerRealtimeNamespaceId),
             completeMatchmakingTriggerScriptId(data.completeMatchmakingTriggerScriptId),
+            status(data.status),
             createdAt(data.createdAt),
             updatedAt(data.updatedAt)
         {
@@ -238,6 +241,13 @@ private:
                     const auto& jsonObject = detail::json::getObject(jsonValue);
                     this->logSetting.emplace();
                     detail::json::JsonParser::parse(&this->logSetting->getModel(), jsonObject);
+                }
+            }
+            else if (std::strcmp(name_, "status") == 0)
+            {
+                if (jsonValue.IsString())
+                {
+                    this->status.emplace(jsonValue.GetString());
                 }
             }
             else if (std::strcmp(name_, "createdAt") == 0)
@@ -748,6 +758,37 @@ public:
     }
 
     /**
+     * Noneを取得
+     *
+     * @return None
+     */
+    const optional<StringHolder>& getStatus() const
+    {
+        return ensureData().status;
+    }
+
+    /**
+     * Noneを設定
+     *
+     * @param status None
+     */
+    void setStatus(StringHolder status)
+    {
+        ensureData().status.emplace(std::move(status));
+    }
+
+    /**
+     * Noneを設定
+     *
+     * @param status None
+     */
+    Namespace& withStatus(StringHolder status)
+    {
+        setStatus(std::move(status));
+        return *this;
+    }
+
+    /**
      * 作成日時を取得
      *
      * @return 作成日時
@@ -881,6 +922,10 @@ inline bool operator!=(const Namespace& lhs, const Namespace& lhr)
             return true;
         }
         if (lhs.m_pData->logSetting != lhr.m_pData->logSetting)
+        {
+            return true;
+        }
+        if (lhs.m_pData->status != lhr.m_pData->status)
         {
             return true;
         }
