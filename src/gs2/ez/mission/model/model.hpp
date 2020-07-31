4f8f18796458e22d4872d41a35b94e1f837e5276
gs2-ci <ci@gs2.io>
2020-07-31 00:13:54 +0000

    [gs2-mission] Service/test seeds modified
    gs2-mission: 7b5ce3d28b2359129530a446b25bf4dc17085f0b
    gs2-sdk-gen: db20f059eb2f3137135a63f0654de8752f269f16
    gs2-test-gen: 38d3257f72df987e3ec2274649015ce7b95a6709

--- src/gs2/ez/mission/model/model.hpp
+++ src/gs2/ez/mission/model/model.hpp
@@ -18,6 +18,8 @@
 #define GS2_EZ_MISSION_MODEL_MODEL_HPP_
 
 #include <gs2/core/common.hpp>
+#include "EzConfig.hpp"
+#include "EzCounter.hpp"
 #include "EzAcquireAction.hpp"
 #include "EzMissionGroupModel.hpp"
 #include "EzCounterModel.hpp"
@@ -25,7 +27,5 @@
 #include "EzComplete.hpp"
 #include "EzCounterScopeModel.hpp"
 #include "EzScopedValue.hpp"
-#include "EzConfig.hpp"
-#include "EzCounter.hpp"
 
 #endif //GS2_EZ_MISSION_MODEL_MODEL_HPP_
