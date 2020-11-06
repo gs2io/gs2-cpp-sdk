9f547f5e40835d829f11253bfe014ee7bc8420a3
gs2-ci <ci@gs2.io>
2020-11-06 03:08:07 +0000

    [gs2-inbox] Service/test seeds modified
    gs2-inbox: 5907839cf28858911569d743486ff1c542ec7c96
    gs2-sdk-gen: 557d4b26f01ddc3fbafdb7a67ee9ac9069e241c9
    gs2-test-gen: f8de733c114f749789754eb2485238c8dafe3d8a

--- src/gs2/inbox/Gs2InboxRestClient.hpp
+++ src/gs2/inbox/Gs2InboxRestClient.hpp
@@ -2305,11 +2305,6 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
-        if (obj.getStatus())
-        {
-            jsonWriter.writePropertyName("status");
-            jsonWriter.writeCharArray(*obj.getStatus());
-        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
--- src/gs2/inbox/model/Namespace.hpp
+++ src/gs2/inbox/model/Namespace.hpp
@@ -72,8 +72,6 @@ private:
         optional<NotificationSetting> receiveNotification;
         /** ログの出力設定 */
         optional<LogSetting> logSetting;
-        /** None */
-        optional<StringHolder> status;
         /** 作成日時 */
         optional<Int64> createdAt;
         /** 最終更新日時 */
@@ -90,7 +88,6 @@ private:
             isAutomaticDeletingEnabled(data.isAutomaticDeletingEnabled),
             queueNamespaceId(data.queueNamespaceId),
             keyId(data.keyId),
-            status(data.status),
             createdAt(data.createdAt),
             updatedAt(data.updatedAt)
         {
@@ -217,13 +214,6 @@ private:
                     const auto& jsonObject = detail::json::getObject(jsonValue);
                     this->logSetting.emplace();
                     detail::json::JsonParser::parse(&this->logSetting->getModel(), jsonObject);
-                }
-            }
-            else if (std::strcmp(name_, "status") == 0)
-            {
-                if (jsonValue.IsString())
-                {
-                    this->status.emplace(jsonValue.GetString());
                 }
             }
             else if (std::strcmp(name_, "createdAt") == 0)
@@ -641,37 +631,6 @@ public:
     }
 
     /**
-     * Noneを取得
-     *
-     * @return None
-     */
-    const optional<StringHolder>& getStatus() const
-    {
-        return ensureData().status;
-    }
-
-    /**
-     * Noneを設定
-     *
-     * @param status None
-     */
-    void setStatus(StringHolder status)
-    {
-        ensureData().status.emplace(std::move(status));
-    }
-
-    /**
-     * Noneを設定
-     *
-     * @param status None
-     */
-    Namespace& withStatus(StringHolder status)
-    {
-        setStatus(std::move(status));
-        return *this;
-    }
-
-    /**
      * 作成日時を取得
      *
      * @return 作成日時
@@ -793,10 +752,6 @@ inline bool operator!=(const Namespace& lhs, const Namespace& lhr)
             return true;
         }
         if (lhs.m_pData->logSetting != lhr.m_pData->logSetting)
-        {
-            return true;
-        }
-        if (lhs.m_pData->status != lhr.m_pData->status)
         {
             return true;
         }
