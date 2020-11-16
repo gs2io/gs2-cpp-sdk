ec135b7b56aff8fe86685ada19d80c0212650493
gs2-ci <ci@gs2.io>
2020-11-16 02:03:54 +0000

    [gs2-showcase] Service/test seeds modified
    gs2-showcase: ca7dabf78692dc0ff8f0820d937f540be09e82c4
    gs2-sdk-gen: 765c10a053895b2ffa084256ae5069d708a87e62
    gs2-test-gen: 9939f6bea94ba9d16b0e9b3df95cfa0105007db9

--- src/gs2/showcase/Gs2ShowcaseRestClient.hpp
+++ src/gs2/showcase/Gs2ShowcaseRestClient.hpp
@@ -2065,11 +2065,6 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
-        if (obj.getStatus())
-        {
-            jsonWriter.writePropertyName("status");
-            jsonWriter.writeCharArray(*obj.getStatus());
-        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
