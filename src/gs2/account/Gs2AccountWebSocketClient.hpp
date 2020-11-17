177afc2bc2f33963b0e0055b76a9a499dcfb5ed0
gs2-ci <ci@gs2.io>
2020-11-17 07:11:04 +0000

    [gs2-account] Service/test seeds modified
    gs2-account: a860e33da9f6d7fab3471f4320269ef13897c633
    gs2-sdk-gen: 19890a9a156c7270e7e9d94d1025028c6ccdaa89
    gs2-test-gen: 9939f6bea94ba9d16b0e9b3df95cfa0105007db9

--- src/gs2/account/Gs2AccountWebSocketClient.hpp
+++ src/gs2/account/Gs2AccountWebSocketClient.hpp
@@ -1670,11 +1670,6 @@ protected:
             jsonWriter.writePropertyName("logSetting");
             write(jsonWriter, *obj.getLogSetting());
         }
-        if (obj.getStatus())
-        {
-            jsonWriter.writePropertyName("status");
-            jsonWriter.writeCharArray(*obj.getStatus());
-        }
         if (obj.getCreatedAt())
         {
             jsonWriter.writePropertyName("createdAt");
